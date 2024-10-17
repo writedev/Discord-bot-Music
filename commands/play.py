aef91b510fc54ca038de6bd24611246857ba3dc2
writedev <locxistwitch@gmail.com>
2024-10-17 13:22:08 +0200

    Titre: Implémentation de la fonctionnalité de lecture audio
    Amélioration du bot musical en ajoutant la possibilité de jouer de la musique à partir de requêtes.  La commande `play` recherche des pistes et des playlists, les ajoute à la file d'attente et lance la lecture automatiquement si aucune piste n'est en cours.

--- commands/play.py
+++ commands/play.py
@@ -8,36 +8,32 @@ from typing import cast
 class Play(commands.Cog):
   def __init__(self, bot : commands.Bot):
     self.bot = bot
-  
-  @commands.command()
-  async def play(self, ctx: Context, url: str):
-    await ctx.send("https://www.youtube.com/watch?v=" + url)
 
 
-    @commands.hybrid_command()  
-    async def play(self, ctx : Context, query):
-        player : wavelink.Player
-        player = cast(wavelink.Player, ctx.voice_client) 
-        if not player:
-            try:
-                player = await ctx.author.voice.channel.connect(cls=wavelink.Player)  # type: ignore
-            except AttributeError:
+  @commands.command()  
+  async def play(self, ctx : Context, query):
+      player : wavelink.Player
+      player = cast(wavelink.Player, ctx.voice_client) 
+      if not player:
+          try:
+              player = await ctx.author.voice.channel.connect(cls=wavelink.Player)  # type: ignore
+          except AttributeError:
                 await ctx.send("Please join a voice channel first before using this command.")
                 return
-            except discord.ClientException:
+          except discord.ClientException:
                 await ctx.send("I was unable to join this voice channel. Please try again.")
                 return
-        tracks: wavelink.Search = await wavelink.Playable.search(query)
-        if isinstance(tracks, wavelink.Playlist):
-                print(f"la playist est {tracks}")
-                await ctx.send(f"Lancement de la playlist **``{tracks}``**")
-        else:
-            track : wavelink.Playable = tracks[0]
-            await player.queue.put_wait(track)
-            await ctx.send(f"Lancement de la chanson **``{track}``**")
+      tracks: wavelink.Search = await wavelink.Playable.search(query)
+      if isinstance(tracks, wavelink.Playlist):
+              print(f"la playist est {tracks}")
+              await ctx.send(f"Lancement de la playlist **``{tracks}``**")
+      else:
+          track : wavelink.Playable = tracks[0]
+          await player.queue.put_wait(track)
+          await ctx.send(f"Lancement de la chanson **``{track}``**")
 
-        if not player.playing:
-            await player.play(player.queue.get(), volume=8, replace=True)
+      if not player.playing:
+          await player.play(player.queue.get(), volume=8, replace=True)
 
 async def setup(bot):
     await bot.add_cog(Play(bot))
